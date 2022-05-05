4ead6ff4975052b685c42ae1fe6f42729992e491
Adriane M Hanson <ahansonuga@gmail.com>
2022-05-05 16:35:21 -0400

    Check script argument

--- format-analysis.py
+++ format-analysis.py
@@ -4,11 +4,28 @@ Purpose: generate format identification and create reports to support:
     2. Assigning processing tiers
     3. Identify risks to address immediately.
 """
+import os
+import sys
 
-# Get accession folder path from script argument and verify it is correct.
-# If not, exits the script.
+# Get accession folder path from script argument, verify it is correct, and make it the current directory.
+# If there is an error, ends the script.
+try:
+    accession = sys.argv[1]
+    os.chdir(accession)
+except IndexError:
+    print("\nThe required script argument is missing.")
+    print("Please run the script again.")
+    print("Script usage: python path/format-analysis.py path/accession_folder")
+    sys.exit()
+except FileNotFoundError:
+    print(f"\nThe provided accession folder '{accession}' is not a valid directory.")
+    print("Please run the script again.")
+    print("Script usage: python path/format-analysis.py path/accession_folder")
+    sys.exit()
 
-# Generate format identification information for the accession.
+# Generate format identification information for the accession using FITS
+# and save the output to a folder in the same parent directory as the accession folder.
+
 
 # Extract select format information for each file, with some data reformatting (PRONOM URL, date, size unit).
 
