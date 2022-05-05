b67ee0277f727022869c44cd09b589db4a148db9
Adriane M Hanson <ahansonuga@gmail.com>
2022-05-05 16:17:16 -0400

    Create format-analysis.py

---
+++ format-analysis.py
@@ -0,0 +1,25 @@
+"""
+Purpose: generate format identification and create reports to support:
+    1. Appraisal (technical and content based)
+    2. Assigning processing tiers
+    3. Identify risks to address immediately.
+"""
+
+# Get accession folder path from script argument and verify it is correct.
+# If not, exits the script.
+
+# Generate format identification information for the accession.
+
+# Extract select format information for each file, with some data reformatting (PRONOM URL, date, size unit).
+
+# Add additional information (file extension, parent folder).
+
+# Add risk information.
+
+# Summarize: by format identification.
+# Summarize: by risk.
+# Summarize: by parent.
+
+# Make subsets based on different risk factors.
+
+# Save reports.
