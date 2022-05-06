70b1c4daac89b0af6a6bc287517b8c522af3302e
Adriane Hanson <ahansonuga@gmail.com>
2022-05-06 08:16:22 -0400

    Update accession variables
    Rename accession to accession_folder, so more clear what this is. Added accession_number, which is needed for naming files later on.

--- format-analysis.py
+++ format-analysis.py
@@ -13,15 +13,15 @@ import configuration as c
 # Get accession folder path from script argument, verify it is correct, and make it the current directory.
 # If there is an error, ends the script.
 try:
-    accession = sys.argv[1]
-    os.chdir(accession)
+    accession_folder = sys.argv[1]
+    os.chdir(accession_folder)
 except IndexError:
     print("\nThe required script argument is missing.")
     print("Please run the script again.")
     print("Script usage: python path/format-analysis.py path/accession_folder")
     sys.exit()
 except FileNotFoundError:
-    print(f"\nThe provided accession folder '{accession}' is not a valid directory.")
+    print(f"\nThe provided accession folder '{accession_folder}' is not a valid directory.")
     print("Please run the script again.")
     print("Script usage: python path/format-analysis.py path/accession_folder")
     sys.exit()
@@ -29,14 +29,17 @@ except FileNotFoundError:
 # Makes a folder for format identification information in the parent directory of the accession folder.
 # If this folder already exists, prints an error and ends the script.
 try:
-    os.mkdir(f"{accession}_FITS")
+    os.mkdir(f"{accession_folder}_FITS")
 except FileExistsError:
-    print(f"There is already FITS data for accession {accession}.")
-    print(f"Delete or move the '{accession}_FITS' folder and run the script again.")
+    print(f"There is already FITS data for accession {accession_folder}.")
+    print(f"Delete or move the '{accession_folder}_FITS' folder and run the script again.")
     sys.exit()
 
 # Generates the format identification information for the accession using FITS
-subprocess.run(f'"{c.FITS}" -r -i "{accession}" -o "{f"{accession}_FITS"}"', shell=True)
+subprocess.run(f'"{c.FITS}" -r -i "{accession_folder}" -o "{f"{accession_folder}_FITS"}"', shell=True)
+
+# Calculates the accession number, which is the name of the last folder in the accession_folder path.
+accession_number = os.path.basename(accession_folder)
 
 # Extract select format information for each file, with some data reformatting (PRONOM URL, date, size unit).
 
