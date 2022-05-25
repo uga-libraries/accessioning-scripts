d1acbb95aeb73739662a1ab55c4be965ac216336
Adriane Hanson <ahansonuga@gmail.com>
2022-05-25 15:46:54 -0400

    Create configuration_template.py
    Guidelines for making the configuration.py file with variables from your local environment.

---
+++ configuration_template.py
@@ -0,0 +1,17 @@
+# Constants used in other scripts that depend on the local environment.
+# To use, save this file as configuration.py in the accessioning-scripts repo.
+
+# For format-analysis.py
+
+# Path to fits.bat (Windows) or fits.sh (Mac/Linux).
+FITS = r""
+
+# Path to ITAfileformats.csv, in your copy of the accessioning-scripts repo.
+ITA = r""
+
+# Path to Riskfileformats.csv, in your copy of the accessioning-scripts repo.
+RISK = r""
+
+# Path to the NARA Preservation Action Plans CSV. Download from:
+# https://github.com/usnationalarchives/digital-preservation/tree/master/Digital_Preservation_Plan_Spreadsheet
+NARA = r""
