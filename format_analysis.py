1bf211c9cd0ebb8b3bdd3e1a48518a2e107f0304
Adriane Hanson <ahansonuga@gmail.com>
2022-08-22 17:50:50 -0400

    Save index in full risk csv
    Temporarily commented out a lot of the script because I need to rework it based on alterations to df_result

--- format_analysis.py
+++ format_analysis.py
@@ -139,72 +139,72 @@ else:
     df_results["Other_Risk"] = df_results["Other_Risk"].fillna(value="Not for Other")
 
     # Saves the information in df_results to a CSV for archivist review.
-    df_results.to_csv(csv_path, index=False)
+    df_results.to_csv(csv_path, index_label="Index")
 
-# Makes subsets based on different risk factors.
-nara_at_risk = df_results[df_results["NARA_Risk Level"] != "Low Risk"].copy()
-multiple_ids = df_results[df_results["FITS_Multiple_IDs"] == True].iloc[:, 0:18].copy()
-multiple_ids.drop(["NARA_Format Name", "NARA_File Extension(s)", "NARA_PRONOM URL"], inplace=True, axis=1)
-multiple_ids.drop_duplicates(inplace=True)
-validation_error = df_results[(df_results["FITS_Valid"] == False) | (df_results["FITS_Well-Formed"] == False) |
-                              (df_results["FITS_Status_Message"].notnull())].copy()
+# Removes duplicates in df_results from multiple NARA matches with the same risk and proposed preservation plan.
+# This information is saved in the accession's full risk data CSV if matches need to be checked.
+df_results.drop(["NARA_Format Name", "NARA_File Extension(s)", "NARA_PRONOM URL"], inplace=True, axis=1)
+df_results.drop_duplicates(inplace=True)
 
-# Makes a subset of files that meet one of the technical appraisal criteria (format or trash folder),
-# including adding a column for which criteria was used.
-# Removes duplicate rows, which are caused by multiple matches to NARA risk criteria.
-columns_list = ["FITS_File_Path", "FITS_Format_Name", "FITS_Format_Version", "FITS_Identifying_Tool(s)",
-                "FITS_Multiple_IDs", "FITS_Size_KB", "FITS_Creating_Application"]
-tech_format = df_results[df_results["Technical Appraisal_Format"] == True][columns_list].copy()
-tech_format.insert(0, "Criteria", "Format")
-tech_trash = df_results[df_results["Technical Appraisal_Trash"] == True][columns_list].copy()
-tech_trash.insert(0, "Criteria", "Trash Folder")
-tech_appraisal = pd.concat([tech_format, tech_trash])
-tech_appraisal.drop_duplicates(inplace=True)
+# # Makes subsets based on different risk factors.
+# nara_at_risk = df_results[df_results["NARA_Risk Level"] != "Low Risk"].copy()
+# multiple_ids = df_results[df_results["FITS_Multiple_IDs"] == True].iloc[:, 0:18].copy()
+# multiple_ids.drop(["NARA_Format Name", "NARA_File Extension(s)", "NARA_PRONOM URL"], inplace=True, axis=1)
+# multiple_ids.drop_duplicates(inplace=True)
+# validation_error = df_results[(df_results["FITS_Valid"] == False) | (df_results["FITS_Well-Formed"] == False) |
+#                               (df_results["FITS_Status_Message"].notnull())].copy()
 
-# Makes a subset of files that meet one of the "other risk" criteria (format or NARA low risk but transform),
-# including adding a column for which criteria was used.
-other_format = df_results[df_results["Other Risk Indicator"] == True][columns_list].copy()
-other_format.insert(0, "Criteria", "Format")
-other_nara_transform = df_results[(df_results["NARA_Risk Level"] == "Low Risk") &
-                                  df_results["NARA_Proposed Preservation Plan"].str.startswith("Transform")][columns_list].copy()
-other_nara_transform.insert(0, "Criteria", "NARA Low Risk/Transform")
-other_risk = pd.concat([other_format, other_nara_transform])
-other_risk.drop_duplicates(inplace=True)
+# # Makes a subset of files that meet one of the technical appraisal criteria (format or trash folder),
+# # including adding a column for which criteria was used.
+# # Removes duplicate rows, which are caused by multiple matches to NARA risk criteria.
+# columns_list = ["FITS_File_Path", "FITS_Format_Name", "FITS_Format_Version", "FITS_Identifying_Tool(s)",
+#                 "FITS_Multiple_IDs", "FITS_Size_KB", "FITS_Creating_Application"]
+# tech_format = df_results[df_results["Technical Appraisal_Format"] == True][columns_list].copy()
+# tech_format.insert(0, "Criteria", "Format")
+# tech_trash = df_results[df_results["Technical Appraisal_Trash"] == True][columns_list].copy()
+# tech_trash.insert(0, "Criteria", "Trash Folder")
+# tech_appraisal = pd.concat([tech_format, tech_trash])
+# tech_appraisal.drop_duplicates(inplace=True)
+#
+# # Makes a subset of files that meet one of the "other risk" criteria (format or NARA low risk but transform),
+# # including adding a column for which criteria was used.
+# other_format = df_results[df_results["Other Risk Indicator"] == True][columns_list].copy()
+# other_format.insert(0, "Criteria", "Format")
+# other_nara_transform = df_results[(df_results["NARA_Risk Level"] == "Low Risk") &
+#                                   df_results["NARA_Proposed Preservation Plan"].str.startswith("Transform")][columns_list].copy()
+# other_nara_transform.insert(0, "Criteria", "NARA Low Risk/Transform")
+# other_risk = pd.concat([other_format, other_nara_transform])
+# other_risk.drop_duplicates(inplace=True)
 
-# Makes a subset of files that are duplicates based on MD5, keeping only a few of the columns.
-# Removes multiple rows for the same file (based on filepath) caused by multiple format identifications
-# or multiple matches to NARA.
-df_duplicates = df_results[["FITS_File_Path", "FITS_Size_KB", "FITS_MD5"]].copy()
-df_duplicates = df_duplicates.drop_duplicates(subset=["FITS_File_Path"], keep=False)
-df_duplicates = df_duplicates.loc[df_duplicates.duplicated(subset="FITS_MD5", keep=False)]
+# # Makes a subset of files that are duplicates based on MD5, keeping only a few of the columns.
+# # Removes multiple rows for the same file (based on filepath) caused by multiple format identifications
+# # or multiple matches to NARA.
+# df_duplicates = df_results[["FITS_File_Path", "FITS_Size_KB", "FITS_MD5"]].copy()
+# df_duplicates = df_duplicates.drop_duplicates(subset=["FITS_File_Path"], keep=False)
+# df_duplicates = df_duplicates.loc[df_duplicates.duplicated(subset="FITS_MD5", keep=False)]
 
-# Makes a version of df_results without duplicates caused by multiple possible NARA matches with the same risk
-# to get more accurate numbers for the subtotals.
-df_results_dedup = df_results.copy()
-df_results_dedup.drop(["NARA_Format Name", "NARA_File Extension(s)", "NARA_PRONOM URL"], inplace=True, axis=1)
-df_results_dedup.drop_duplicates(inplace=True)
 
-# Calculates the total files and total size in the dataframe to use for percentages with the subtotals.
-totals_dict = {"Files": len(df_results_dedup.index), "MB": df_results_dedup["FITS_Size_KB"].sum()/1000}
-
-# Calculates file and size subtotals based on different criteria.
-format_subtotals = subtotal(df_results_dedup, ["FITS_Format_Name", "NARA_Risk Level"], totals_dict)
-nara_risk_subtotals = subtotal(df_results_dedup, ["NARA_Risk Level"], totals_dict)
-technical_appraisal_subtotals = subtotal(tech_appraisal, ["Criteria", "FITS_Format_Name"], totals_dict)
-other_risk_subtotals = subtotal(other_risk, ["Criteria", "FITS_Format_Name"], totals_dict)
-media_subtotals = media_subtotal(df_results_dedup, accession_folder)
+# # Calculates the total files and total size in the dataframe to use for percentages with the subtotals.
+# totals_dict = {"Files": len(df_results_dedup.index), "MB": df_results_dedup["FITS_Size_KB"].sum()/1000}
+#
+# # Calculates file and size subtotals based on different criteria.
+# format_subtotals = subtotal(df_results_dedup, ["FITS_Format_Name", "NARA_Risk Level"], totals_dict)
+# nara_risk_subtotals = subtotal(df_results_dedup, ["NARA_Risk Level"], totals_dict)
+# technical_appraisal_subtotals = subtotal(tech_appraisal, ["Criteria", "FITS_Format_Name"], totals_dict)
+# other_risk_subtotals = subtotal(other_risk, ["Criteria", "FITS_Format_Name"], totals_dict)
+# media_subtotals = media_subtotal(df_results_dedup, accession_folder)
 
 # Saves all dataframes to a separate tab in an Excel spreadsheet in the collection folder.
 # The index is not included if it is the row numbers.
-with pd.ExcelWriter(f"{collection_folder}/{accession_number}_format-analysis.xlsx") as result:
-    format_subtotals.to_excel(result, sheet_name="Format Subtotals")
-    nara_risk_subtotals.to_excel(result, sheet_name="NARA Risk Subtotals")
-    technical_appraisal_subtotals.to_excel(result, sheet_name="Tech Appraisal Subtotals")
-    #other_risk_subtotals.to_excel(result, sheet_name="Other Risk Subtotals")
-    media_subtotals.to_excel(result, sheet_name="Media Subtotals", index_label="Media")
-    nara_at_risk.to_excel(result, sheet_name="NARA Risk", index=False)
-    tech_appraisal.to_excel(result, sheet_name="For Technical Appraisal", index=False)
-    other_risk.to_excel(result, sheet_name="Other Risks", index=False)
-    multiple_ids.to_excel(result, sheet_name="Multiple Formats", index=False)
-    df_duplicates.to_excel(result, sheet_name="Duplicates", index=False)
-    validation_error.to_excel(result, sheet_name="Validation", index=False)
+# with pd.ExcelWriter(f"{collection_folder}/{accession_number}_format-analysis.xlsx") as result:
+#     format_subtotals.to_excel(result, sheet_name="Format Subtotals")
+#     nara_risk_subtotals.to_excel(result, sheet_name="NARA Risk Subtotals")
+#     technical_appraisal_subtotals.to_excel(result, sheet_name="Tech Appraisal Subtotals")
+#     other_risk_subtotals.to_excel(result, sheet_name="Other Risk Subtotals")
+#     media_subtotals.to_excel(result, sheet_name="Media Subtotals", index_label="Media")
+#     nara_at_risk.to_excel(result, sheet_name="NARA Risk", index=False)
+#     tech_appraisal.to_excel(result, sheet_name="For Technical Appraisal", index=False)
+#     other_risk.to_excel(result, sheet_name="Other Risks", index=False)
+#     multiple_ids.to_excel(result, sheet_name="Multiple Formats", index=False)
+#     df_duplicates.to_excel(result, sheet_name="Duplicates", index=False)
+#     validation_error.to_excel(result, sheet_name="Validation", index=False)
