c1ee71018b5282543242808cdf89ed313763e152
Adriane Hanson <ahansonuga@gmail.com>
2022-12-12 15:36:51 -0500

    media: add fill zero test

--- tests/test_media_subtotal.py
+++ tests/test_media_subtotal.py
@@ -326,6 +326,32 @@ class MyTestCase(unittest.TestCase):
         # Compares the results. assertEqual prints "OK" or the differences between the two lists.
         self.assertEqual(results, expected, 'Problem with rounding')
 
+    def test_fill_zero(self):
+        """
+        Test for filling columns with 0 if there were no file of that type.
+        This is identical to other risk, none have risk.
+        It is repeated as a test for 0 to highlight when the zero fill might be a problem.
+        Result for testing is the media subtotal dataframe, converted to a list for easier comparison.
+        """
+        # Makes a dataframe to use as test input and runs the function being tested.
+        rows = [['C:\\ACC\\Disk1\\file1.txt', 1000, 'Low Risk', 'Not for TA', 'Not for Other'],
+                ['C:\\ACC\\Disk1\\file2.txt', 2000, 'Low Risk', 'Not for TA', 'Not for Other'],
+                ['C:\\ACC\\Disk1\\file3.txt', 3000, 'Low Risk', 'Not for TA', 'Not for Other'],
+                ['C:\\ACC\\Disk2\\file4.txt', 4000, 'Low Risk', 'Not for TA', 'Not for Other'],
+                ['C:\\ACC\\Disk2\\file5.txt', 5000, 'Low Risk', 'Not for TA', 'Not for Other']]
+        column_names = ['FITS_File_Path', 'FITS_Size_KB', 'NARA_Risk Level', 'Technical_Appraisal', 'Other_Risk']
+        df_results = pd.DataFrame(rows, columns=column_names)
+        df_media_subtotal = media_subtotal(df_results, self.accession_folder)
+
+        # Makes lists of the actual results from the test and the expected results.
+        # Uses reset_index() to include the index value in the dataframe, which is the media name.
+        results = df_media_subtotal.reset_index().values.tolist()
+        expected = [['Disk1', 3, 6, 0, 0, 3, 0, 0, 0],
+                    ['Disk2', 2, 9, 0, 0, 2, 0, 0, 0]]
+
+        # Compares the results. assertEqual prints "OK" or the differences between the two lists.
+        self.assertEqual(results, expected, 'Problem with fill zero')
+
 
 if __name__ == '__main__':
     unittest.main()
